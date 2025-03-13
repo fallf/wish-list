d447131d7ff823523177556cb1e539e14fe65b6d
fatou fall <fatoufall95@gmail.com>
2025-03-13 16:00:18 -0400

    started frontend

--- backend/server.js
+++ backend/server.js
@@ -9,21 +9,22 @@ import productRoutes from "./routes/productRoutes.mjs";
 
 const app = express();
 
+const PORT = process.env.PORT || 5032;
 app.use(express.json()); // Allows us to accept JSON data in req.body
 
 //mddleware
 app.use(detailedLogger);
 
 // ✅ Check if `MONGO_URI` is missing and prevent crashes
-if (!process.env.MONGO_URI) {
-  console.error("❌ ERROR: MONGO_URI is not defined in the .env file.");
-  process.exit(1); // Stop the app if the database URI is missing
-}
+// if (!process.env.MONGO_URI) {
+//   console.error("❌ ERROR: MONGO_URI is not defined in the .env file.");
+//   process.exit(1); // Stop the app if the database URI is missing
+// }
 
 app.use("/api/products", productRoutes);
 
 // ✅ Start Server & Connect to MongoDB
-app.listen(5030, async () => {
+app.listen(PORT, async () => {
   await connectDB(process.env.MONGO_URI); // ✅ Pass the variable directly
-  console.log("Server started at port 5030");
+  console.log("Server started at http://localhost:" + PORT);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button, Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={4}>
+      <Text fontSize="xl">Hello, Chakra UI!</Text>
+      <Button colorScheme="blue" mt={4}>
+        Click Me
+      </Button>
+    </Box>
+  );
+}
+
+export default App;
