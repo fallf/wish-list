7f2a1fd08c3a7f65945a923dd3d7a59ed62e38e9
fatou fall <fatoufall95@gmail.com>
2025-03-14 15:04:43 -0400

    created a form to add items

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,83 @@
+import {
+  Button,
+  Container,
+  Box,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 
 function CreatePage() {
-  return <div>CreatePage</div>;
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+    link: "",
+  });
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2x1"} textAlign={"center"} mb={8}>
+          Add Your Desired Item✨
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Item Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.imagemage}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Item Link"
+              name="link"
+              value={newProduct.link}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, link: e.target.value })
+              }
+            />
+            <Button colorScheme="pink" onClick={handleAddProduct} w="full">
+              {" "}
+              Add Item{" "}
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 }
 
 export default CreatePage;
